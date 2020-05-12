4c2372c22973e0fb103faf90420f2924394feaf9
ravi <ravi.periketi@fxinnovation.com>
2020-05-12 13:44:53 -0400

    fix typo in output

--- outputs.tf
+++ outputs.tf
@@ -4,10 +4,10 @@
 
 output "id" {
   description = "The Resource ID of the Azure firewall"
-  value       = element(concat(azurem_firewall.this.*.id, [""]), 0)
+  value       = element(concat(azurerm_firewall.this.*.id, [""]), 0)
 }
 
 output "ip_configuration" {
   description = "The Ip configuration of the Azure firewall."
-  value       = azurem_firewall.this.*.ip_configuration
+  value       = azurerm_firewall.this.*.ip_configuration
 }
